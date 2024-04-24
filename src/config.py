cc8f86d2913642b0cb37905fe2b9f8c97a9c4c28
JustBSI <just.b.sergey@gmail.com>
2024-04-24 04:34:36 +0300

    All done right now exactly.

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DOCKER = os.getenv("DOCKER")
-DB_HOST = os.environ.get("DB_HOST") if not DOCKER else os.getenv("DOCKER_HOST")
+DOCKER = os.environ.get("DOCKER")
+DB_HOST = os.environ.get("DB_HOST") if not DOCKER else os.environ.get("DOCKER_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
